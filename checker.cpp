16b04019ab9b9d8d14d86e9fb893a44a8700d7d6
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-27 11:17:42 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,24 @@
 #include <assert.h>
 
+bool bpm_is_ok(float bpm)
+{
+    return !(bpm < 70 || bpm > 150);
+}
+bool spo2_is_ok(float spo2)
+{
+    return !(spo2 < 90);
+}
+bool respRate_is_ok(float respRate)
+{
+    return !(respRate < 30 || respRate > 95);
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+    return(bpm_is_ok(bpm) && spo2_is_ok(spo2) && respRate_is_ok(respRate));
 }
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+    assert(vitalsAreOk(80, 95, 60) == true);
+    assert(vitalsAreOk(60, 90, 40) == false);
 }
+
