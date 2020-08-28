6d1faee3a68f40c4e4fa347add7278e9a116ffe0
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-08-28 09:24:51 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,24 +1,39 @@
-#include <assert.h>
+#include <iostream>
+#include<assert.h>
+using namespace std;
 
-bool bpm_is_ok(float bpm)
-{
-    return !(bpm < 70 || bpm > 150);
-}
-bool spo2_is_ok(float spo2)
-{
-    return !(spo2 < 90);
-}
-bool respRate_is_ok(float respRate)
-{
-    return !(respRate < 30 || respRate > 95);
-}
+struct vitals {
+    float bpm;
+    int spo2;
+    int respRate;
+};
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-    return(bpm_is_ok(bpm) && spo2_is_ok(spo2) && respRate_is_ok(respRate));
-}
+bool vitalsAreOk(vitals);
+bool cardiacIsOk(float, int);
+bool respIsOk(int);
 
 int main() {
-    assert(vitalsAreOk(80, 95, 60) == true);
-    assert(vitalsAreOk(60, 90, 40) == false);
+    vitals pat1 = { 60,95,40 };
+    assert(vitalsAreOk(pat1) == false);
+    vitals pat2 = { 80,85,40 };
+    assert(vitalsAreOk(pat2) == false);
+    vitals pat3 = { 80,95,125 };
+    assert(vitalsAreOk(pat3) == false);
+    vitals pat4 = { 80,95,40 };
+    assert(vitalsAreOk(pat4) == true);
+    cout << "ok!!";
+    return 0;
+}
+bool cardiacIsOk(float bpm, int spo2) {
+    return(bpm > 70 && bpm < 150 && spo2 > 90 && spo2 < 100);
+}
+
+bool respIsOk(int respRate) {
+    return(respRate > 30 && respRate < 95);
+}
+
+bool vitalsAreOk(vitals pat1)
+{
+    return(cardiacIsOk(pat1.bpm, pat1.spo2) && respIsOk(pat1.respRate));
 }
 
